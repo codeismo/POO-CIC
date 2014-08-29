b6c90f7c7d018912139e7d669053d2031320a213
Daniela Aguirre <danilea@codeismo.com>
2014-08-29 09:16:33 -0500

    gatito.c

---
+++ C/gatito.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*El tablero es una variable global*/
+char tablero[9] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+
+/*Imprime el tablero así
+- - -
+- - -
+- - -
+
+*/
+void imprimir_tablero(){
+
+}
+
+/*Pide al usuario la posicion y el simbolo para tirar
+modifica el tablero con el tiro del jugador*/
+void tirar(){
+
+}
+
+/*Revisa en el tablero y si alguien gana devuelve 1
+Si el tablero esta lleno devuelve 0 y si el juego
+continua devuelve 2*/
+int ganar(){
+
+}
+
+
+int main(){
+
+    return 0;
+}
