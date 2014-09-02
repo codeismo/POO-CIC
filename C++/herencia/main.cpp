0e95459e034e600621bb0450acd11d9e9f64884a
Daniela Aguirre <danilea@codeismo.com>
2014-09-01 20:38:32 -0500

    agregue clase Pollo

--- C++/herencia/main.cpp
+++ C++/herencia/main.cpp
@@ -12,10 +12,11 @@ public:
     Animal(string n, int e);
     string getNombre();
     int getEdad();
+    void comer();
 };
  Animal::Animal(){
     nombre = "Animalito";
-    edad = 0;
+    edad = 10;
  }
  Animal::Animal(string n, int e){
     nombre = n;
@@ -27,28 +28,66 @@ public:
 int Animal::getEdad(){
     return edad;
 }
- /*** Clase Gato Hereda de Animal***/
+
+void Animal::comer(){
+    cout<<"Animal comiendo"<<endl;
+}
+ /*** Overeading Clase Gato Hereda de Animal***/
  class Gato:public Animal{
 private:
     string color;
 public:
     Gato();
-    Gato(string c);
+    Gato(string c, string n, int e);
     string getColor();
+    void mostrarInfo();
+    void comer();
  };
 
 Gato::Gato(){
     color = "azul";
 }
-Gato::Gato(string c){
+Gato::Gato(string c, string n, int e):Animal(n, e){
     color = c;
 }
 string Gato::getColor(){
     return color;
 }
 
+void Gato::mostrarInfo(){
+    /** Es privado pero es propio de la Clase Gato **/
+    cout<<"Color del gato "<<(*this).color<<endl;
+    /** Es protegido y lo heredo de la Clase Animal**/
+    cout<<"Nombre del gato "<<this->nombre<<endl;
+    /** Lo heredo de la clase Animal pero es privado
+    cout<<"Edad del gato "<<(*this).edad<<endl;**/
+    cout<<"Edad del gato "<<(*this).getEdad()<<endl;
+}
+
+/*** Sobrecarga de Métodos ***/
+void Gato::comer(){
+    cout<<"miau miau"<<endl;
+}
+
+/*** Clase Pollo ***/
+class Pollo:public Animal{
+public:
+    Pollo();
+    Pollo(string n, int e);
+    void comer();
+};
+Pollo::Pollo(){
+    nombre = "Pollito";
+}
+Pollo::Pollo(string n, int e):Animal(n, e){
+}
+void Pollo::comer(){
+    cout<<"pio pio"<<endl;
+}
 int main()
 {
-    Gato gatito;
+    Gato gatito("amarillo", "Felix", 3);
+    gatito.mostrarInfo();
+    gatito.comer();
     return 0;
 }
