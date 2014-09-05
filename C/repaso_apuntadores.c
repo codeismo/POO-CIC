c95f3358e433d93f756a8ef856d6d18233c8954c
Daniela Aguirre <danilea@codeismo.com>
2014-09-05 09:11:46 -0500

    reapso apunador

--- C/repaso_apuntadores.c
+++ C/repaso_apuntadores.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 /** Variables globales
 son visibles para todas las funciones **/
-int x=0, y=0;
+int y=0;
 
-void cambiaValorX(){
+/** El parametro x es una variable local
+SOLO ES VISIBLE PARA cambiaX**/
+/** Recibe parametros por valor
+(le estoy enviando el valor de x) **/
+void cambiaX(int x){
     x = 10;
+    printf("Funcion: x vive en %p\n", &x);
     printf("Funcion: nuevo valor de x es %d\n", x);
 }
 
+/** Recibe la direccion de memoria de x
+(funcion que recibe parametros por referencia) **/
+void cambiaXPorReferencia( int *ptrX ){
+    /** Los apuntadores guardan direcciones de memoria **/
+    printf("Funcion por Referencia: La direccion de x %p\n", ptrX);
+    *ptrX = 10;
+    printf("Funcion por Referencia: El nuevo valor almacenado en x es %d\n", *ptrX);
+}
+
 int main(){
-    /** **/
+    /**Variable local es visible solo para el main**/
+    int x=0;
+    printf("Main: x vive en %p\n", &x);
     printf("Main: Valor inicial de x es %d\n", x);
-    cambiaValorX();
+    cambiaX( x );
+    printf("Main: Despues de cambiaX() x es %d\n", x);
+    cambiaXPorReferencia( &x );
+    printf("Main: Despues de cambiaXPorReferencia() x es %d\n", x);
     return 0;
 }
