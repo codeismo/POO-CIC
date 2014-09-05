2bbce2e5ba9152b26aaf557f1d5a294eb9c6e1fd
Daniela Aguirre <danilea@codeismo.com>
2014-09-04 19:29:18 -0500

    proyecto POO_Java

---
+++ Java/POO_Java/src/com/codeismo/Animal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.codeismo;
+
+/**
+ *
+ * @author Aula E3
+ */
+public class Animal {
+    /*Definir los atributos de la clase*/
+    /*Definir los metdos de la clase*/
+}
---
+++ Java/POO_Java/src/poo_java/POO_Java.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package poo_java;
+
+/**
+ *
+ * @author Aula E3
+ */
+public class POO_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
