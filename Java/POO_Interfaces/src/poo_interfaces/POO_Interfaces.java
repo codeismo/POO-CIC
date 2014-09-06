d7a7625ceb20f9f0f6c69e65c96705fc015a608c
Daniela Aguirre <danilea@codeismo.com>
2014-09-05 19:47:52 -0500

    diagram de clases y requerimeintos Java

---
+++ Java/POO_Interfaces/src/poo_interfaces/POO_Interfaces.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package poo_interfaces;
+
+/**
+ *
+ * @author Aula E3
+ */
+public class POO_Interfaces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
