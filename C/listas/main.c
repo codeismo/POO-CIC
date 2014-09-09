e6c69d6ab64495354a8ca60a9915187e76acc437
Daniela Aguirre <danilea@codeismo.com>
2014-09-09 08:33:46 -0500

    estructuras compuesta

--- C/listas/main.c
+++ C/listas/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+//struct Persona es un tipo de dato
 struct Persona{
+    //mienbros de la estructura persona
     char nombre[20];
     int edad;
 };
@@ -14,10 +16,13 @@ struct Viaje{
 int main(){
     /*Declarando e inicializando
      una estructura de tipo Cliente y Viaje*/
-    struct Cliente unCliente = {"Daniela", 30};
+    struct Persona unCliente = {"Daniela", 30};
     struct Viaje paseo = {unCliente, "Acapulco", 3};
+    //imprimir el destino usando viaje
+    printf("Destino: %s \n", paseo.destino );
     //Usando la variable paseo imprime el
     //nombre del cliente
+
 
     return 0;
 }
