cee88a03695c0caf36ff3b058523e4ddcc465cf2
Daniela Aguirre <danilea@codeismo.com>
2014-09-09 08:23:39 -0500

    estructuras compuesta

---
+++ C/listas/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Persona{
+    char nombre[20];
+    int edad;
+};
+/*Estructura compuesta*/
+struct Viaje{
+    struct Persona cliente;
+    char destino[20];
+    int duracion;
+};
+
+int main(){
+    /*Crear una estructura de tipo cliente*/
+    return 0;
+}
