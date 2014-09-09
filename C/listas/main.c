dce81a957cd853448eed75e4ce6be47860f0bdd8
Daniela Aguirre <danilea@codeismo.com>
2014-09-09 09:23:57 -0500

    estructuras compuesta

--- C/listas/main.c
+++ C/listas/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+struct Reservacion{
+    char hotel[20];
+    float costo;
+};
 //struct Persona es un tipo de dato
 struct Persona{
     //mienbros de la estructura persona
@@ -9,6 +13,7 @@ struct Persona{
 /*Estructura compuesta*/
 struct Viaje{
     struct Persona cliente;
+    struct Reservacion datosDeReservacion;
     char destino[20];
     int duracion;
 };
@@ -17,7 +22,18 @@ int main(){
     /*Declarando e inicializando
      una estructura de tipo Cliente y Viaje*/
     struct Persona unCliente = {"Daniela", 30};
-    struct Viaje paseo = {unCliente, "Acapulco", 3};
+    struct Reservacion unaReservacion = {"Marriot", 1000};
+    struct Viaje paseo = {unCliente, unaReservacion, "Acapulco", 3};
+
+    /*** Imprimir usando la variable de tipo
+    estructura Viaje (paseo) ***/
+    printf("Destino %s\n", paseo.destino);
+    printf("Nombre cliente: %s\n", paseo.cliente.nombre);
+    printf("Edad: %d\n", paseo.cliente.edad);
+    printf("Hotel: %s \n", paseo.datosDeReservacion.hotel);
+    printf("Costo: %f \n", paseo.datosDeReservacion.costo);
+
+
     //imprimir el destino usando viaje
     printf("Destino: %s \n", paseo.destino );
     //imprimir el nobre del cliente usando unCliente
